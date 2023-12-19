05e03c50e76ac6b96a3f95dd4e94bb2d658dbeb9
John Mampilli <jsmampilli@gmail.com>
2023-12-19 20:15:40 +0100

    Removed unused lines, added comments

--- examples/simple_cycle/hydraulic.jl
+++ examples/simple_cycle/hydraulic.jl
@@ -19,15 +19,18 @@ function pipe_edge!(de, e, v_s, v_d, p, _)
     velocity= e[1] / (p.density * area)
     Re= p.density * velocity * p.diameter / p.dyn_visc
 
-    # @show Re
-    # @show velocity
-
-    de[1] = p.density * area * ( (v_s[1] - v_d[1]) + (p.friction_fn(Re) * velocity * abs(velocity)) ) # Pressure drop due to friction
+    # e[1] : mass flow rate, algebraic constraint
+    #   => de[1] == 0, used to calculate pressure drop across pipe due to friction
+    # TODO: Implement pressure loss according to Cengel eqn. 8-21,
+    #       Churchill or Swamee-Jain approximation for Darcy-Weisbach f
+    de[1] = (v_s[1] - v_d[1]) + (p.friction_fn(Re) * velocity * abs(velocity)) # Pressure drop due to friction
 
     return nothing
 end
 
 function prosumer_edge!(de, e, v_s, v_d, p, _)
+    # de[1] == 0, enforce fixed pressure drop or massflow across edge to model pump/heat exchanger
+
     # de[1] = p.delta_p - (v_d[1] - v_s[1]) # Fixed pressure difference
     de[1] = p.massflow - e[1] # Fixed mass flow rate
 
