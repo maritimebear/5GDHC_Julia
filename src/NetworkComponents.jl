ac60b57872c563f91ed2c435e716643fe9ce4b06
John Mampilli <jsmampilli@gmail.com>
2023-12-31 19:11:26 +0100

    Structs to interface between parsed network and DHG.jl functions

---
+++ src/NetworkComponents.jl
@@ -0,0 +1,39 @@
+module NetworkComponents
+
+
+export Node, Edge, ProsumerEdge, Junction, FixedNode, Pipe, PressureChangeProsumer, MassflowProsumer
+
+
+abstract type Node end
+abstract type Edge end
+abstract type ProsumerEdge <: Edge end
+
+
+struct Junction <: Node
+    # No attributes for junction nodes
+end
+
+
+struct FixedNode <: Node
+    pressure::Float64
+    temperature::Float64
+end
+
+
+struct Pipe <: Edge
+    # TODO
+end
+
+
+struct PressureChangeProsumer <: ProsumerEdge
+    delta_p::Float64
+    delta_T::Float64
+end
+
+
+struct MassflowProsumer <: ProsumerEdge
+    massflow::Float64
+end
+
+
+end # module
