4c105656e4b186f87a475cc9a561c33ce0127dd7
John Mampilli <jsmampilli@gmail.com>
2024-01-03 18:37:14 +0100

    Renamed file

--- src/DHG.jl
+++ src/DHG.jl
@@ -3,12 +3,14 @@ module DHG
 import NetworkDynamics
 import LinearAlgebra
 
-include("./InterpolationSchemes.jl")    # provides submodule FVM
+include("./FVM.jl")                     # provides submodule FVM
 include("./Utilities.jl")               # submodule Utilities
 include("./Physics.jl")                 # submodule Physics
 include("./WrapperFunctions.jl")        # submodule WrapperFunctions
 include("./NetworkComponents.jl")       # submodule NetworkComponents
 include("./GraphParsing.jl")            # submodule GraphParsing
 include("./ParameterStructs.jl")        # submodule ParameterStructs
+
+# TODO: export parse_gml, others?
 
 end # module DHG
