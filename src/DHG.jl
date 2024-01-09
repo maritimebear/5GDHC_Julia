549ceb1958c578876e0bc483261d96c0e363c02c
John Mampilli <jsmampilli@gmail.com>
2024-01-09 20:59:02 +0100

    DHGStruct to interface between various data structures

--- src/DHG.jl
+++ src/DHG.jl
@@ -15,7 +15,44 @@ include("./ParameterStructs.jl")        # submodule ParameterStructs
 import .TransportProperties: TransportCoefficients
 import .ParameterStructs: NodeParameters, EdgeParameters, GlobalParameters, Parameters
 
+import Graphs
+import NetworkDynamics
+
 export TransportCoefficients
 export NodeParameters, EdgeParameters, GlobalParameters, Parameters
+
+
+Base.@kwdef struct DHGStruct{IndexType <: Integer}
+    node_functions::Vector{NetworkDynamics.DirectedODEVertex}
+    edge_functions::Vector{NetworkDynamics.ODEEdge}
+    parameters::ParameterStructs.Parameters
+    graph::Graphs.SimpleGraphs.SimpleDiGraph{IndexType}
+end
+
+
+function DHGStruct(graph_parser::Function,
+                    global_parameters::ParameterStructs.GlobalParameters,
+                    transport_coeffs::TransportProperties.TransportCoefficients)
+    # Constructor
+
+    graph, node_dict, edge_dict = graph_parser()
+    parameters = ParameterStructs.Parameters(global_parameters = global_parameters,
+                                             node_parameters = ParameterStructs.NodeParameters(node_dict),
+                                             edge_parameters = ParameterStructs.EdgeParameters(edge_dict)
+                                            )
+    # Assemble dynamical functions
+    node_fns = Vector{NetworkDynamics.DirectedODEVertex}(undef, length(node_dict.components))
+    edge_fns = Vector{NetworkDynamics.ODEEdge}(undef, length(edge_dict.components))
+
+    for (i, node) in enumerate(node_dict.components)
+        node_fns[i] = WrapperFunctions.node_fn(node)
+    end
+
+    for (i, edge) in enumerate(edge_dict.components)
+        edge_fns[i] = WrapperFunctions.edge_fn(i, edge, transport_coeffs)
+    end
+
+    return DHGStruct(node_functions=node_fns, edge_functions=edge_fns, parameters=parameters, graph=graph)
+end
 
 end # module DHG
