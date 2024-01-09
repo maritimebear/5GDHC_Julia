7e97c3bf79de8bf223ef8d869bab55aabe943e0f
John Mampilli <jsmampilli@gmail.com>
2024-01-09 17:31:19 +0100

    Reordered include order according to mutual dependencies

--- src/DHG.jl
+++ src/DHG.jl
@@ -1,11 +1,11 @@
 module DHG
 
-include("./FVM.jl")                     # provides submodule FVM
-include("./TransportProperties.jl")     # submodule TransportProperties
 include("./Utilities.jl")               # submodule Utilities
+include("./FVM.jl")                     # provides submodule FVM
+include("./NetworkComponents.jl")       # submodule NetworkComponents
+include("./TransportProperties.jl")     # submodule TransportProperties
 include("./DynamicalFunctions.jl")      # submodule DynamicalFunctions
 include("./WrapperFunctions.jl")        # submodule WrapperFunctions
-include("./NetworkComponents.jl")       # submodule NetworkComponents
 include("./GraphParsing.jl")            # submodule GraphParsing
 include("./ParameterStructs.jl")        # submodule ParameterStructs
 
