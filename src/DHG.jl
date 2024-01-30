5e8740d03942ce18123ea7d5673bac18f67a7f3d
John Mampilli <jsmampilli@gmail.com>
2024-01-30 18:37:10 +0100

    Commented out unused code

--- src/DHG.jl
+++ src/DHG.jl
@@ -3,68 +3,67 @@ module DHG
 include("./Utilities.jl")               # submodule Utilities
 include("./FVM.jl")                     # provides submodule FVM
 include("./NetworkComponents.jl")       # submodule NetworkComponents
-include("./TransportProperties.jl")     # submodule TransportProperties
+include("./Transport.jl")               # submodule TransportProperties
 include("./DynamicalFunctions.jl")      # submodule DynamicalFunctions
 include("./WrapperFunctions.jl")        # submodule WrapperFunctions
 include("./GraphParsing.jl")            # submodule GraphParsing
-include("./ParameterStructs.jl")        # submodule ParameterStructs
+include("./ParameterStruct.jl")         # submodule ParameterStructs
 
-# TODO: export parse_gml, others?
 # TODO: global size_t, but NetworkDynamics takes dims::Int
 
-import Graphs: SimpleGraphs.SimpleDiGraph
-import NetworkDynamics
-import DifferentialEquations: SciMLBase
+# import Graphs: SimpleGraphs.SimpleDiGraph
+# import NetworkDynamics
+# import DifferentialEquations: SciMLBase
 
 # import and re-export from submodules for easier access
 import .GraphParsing: parse_gml
-import .ParameterStructs: GlobalParameters
-import .TransportProperties: TransportCoefficients
+import .ParameterStruct: Parameters
+import .Transport: TransportProperties
 
-export GlobalParameters, TransportProperties, parse_gml
+export Parameters, TransportProperties, parse_gml
 
-export DHGStruct
-
-
-Base.@kwdef struct DHGStruct{IndexType <: Integer, FunctionType <: SciMLBase.AbstractODEFunction}
-    f::FunctionType
-    parameters::ParameterStructs.Parameters
-    graph::SimpleDiGraph{IndexType}
-    n_states::NamedTuple{(:nodes, :edges),
-                         Tuple{Vector{UInt16}, Vector{UInt16}} # Determines max number of states, change in constructor as well if modified
-                        }
-end
+# export DHGStruct
 
 
-function DHGStruct(graph_parser::Function,
-                    global_parameters::ParameterStructs.GlobalParameters,
-                    transport_coeffs::TransportProperties.TransportCoefficients,
-                )
-    # Constructor
+# Base.@kwdef struct DHGStruct{IndexType <: Integer, FunctionType <: SciMLBase.AbstractODEFunction}
+#     f::FunctionType
+#     parameters::ParameterStructs.Parameters
+#     graph::SimpleDiGraph{IndexType}
+#     n_states::NamedTuple{(:nodes, :edges),
+#                          Tuple{Vector{UInt16}, Vector{UInt16}} # Determines max number of states, change in constructor as well if modified
+#                         }
+# end
 
-    graph, node_dict, edge_dict = graph_parser()
-    parameters = ParameterStructs.Parameters(global_parameters = global_parameters,
-                                             node_parameters = ParameterStructs.NodeParameters(node_dict),
-                                             edge_parameters = ParameterStructs.EdgeParameters(edge_dict)
-                                            )
-    # Assemble dynamical functions
-    node_fns = Vector{NetworkDynamics.DirectedODEVertex}(undef, length(node_dict.components))
-    edge_fns = Vector{NetworkDynamics.ODEEdge}(undef, length(edge_dict.components))
-    n_states = (nodes=similar(node_fns, UInt16), edges=similar(edge_fns, UInt16))
 
-    for (i, node) in enumerate(node_dict.components)
-        node_fns[i] = WrapperFunctions.node_fn(node)
-        n_states.nodes[i] = node_fns[i].dim
-    end
+# function DHGStruct(graph_parser::Function,
+#                     global_parameters::ParameterStructs.GlobalParameters,
+#                     transport_coeffs::TransportProperties.TransportCoefficients,
+#                 )
+#     # Constructor
 
-    for (i, edge) in enumerate(edge_dict.components)
-        edge_fns[i] = WrapperFunctions.edge_fn(i, edge, transport_coeffs)
-        n_states.edges[i] = edge_fns[i].dim
-    end
+#     graph, node_dict, edge_dict = graph_parser()
+#     parameters = ParameterStructs.Parameters(global_parameters = global_parameters,
+#                                              node_parameters = ParameterStructs.NodeParameters(node_dict),
+#                                              edge_parameters = ParameterStructs.EdgeParameters(edge_dict)
+#                                             )
+#     # Assemble dynamical functions
+#     node_fns = Vector{NetworkDynamics.DirectedODEVertex}(undef, length(node_dict.components))
+#     edge_fns = Vector{NetworkDynamics.ODEEdge}(undef, length(edge_dict.components))
+#     n_states = (nodes=similar(node_fns, UInt16), edges=similar(edge_fns, UInt16))
 
-    ode_fn = NetworkDynamics.network_dynamics(node_fns, edge_fns, graph)
+#     for (i, node) in enumerate(node_dict.components)
+#         node_fns[i] = WrapperFunctions.node_fn(node)
+#         n_states.nodes[i] = node_fns[i].dim
+#     end
 
-    return DHGStruct(f=ode_fn, parameters=parameters, graph=graph, n_states=n_states)
-end
+#     for (i, edge) in enumerate(edge_dict.components)
+#         edge_fns[i] = WrapperFunctions.edge_fn(i, edge, transport_coeffs)
+#         n_states.edges[i] = edge_fns[i].dim
+#     end
+
+#     ode_fn = NetworkDynamics.network_dynamics(node_fns, edge_fns, graph)
+
+#     return DHGStruct(f=ode_fn, parameters=parameters, graph=graph, n_states=n_states)
+# end
 
 end # module DHG
