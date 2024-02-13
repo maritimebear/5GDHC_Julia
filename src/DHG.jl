e71b029a8210df873c61f15c23a416cd2054f8f6
John Mampilli <jsmampilli@gmail.com>
2024-02-13 18:43:19 +0100

    Modified imports for convenient access

--- src/DHG.jl
+++ src/DHG.jl
@@ -2,9 +2,14 @@ module DHG
 
 include("FVM.jl")
 include("Transport.jl")
+include("ControlFunctions.jl")
 include("NetworkComponents.jl")
 include("DynamicalFunctions.jl")
 include("WrapperFunctions.jl")
+
+import .NetworkComponents: Node, Edge, Prosumer, JunctionNode, ReferenceNode, Pipe, PressureChange, Massflow
+import .Transport: TransportProperties
+import .WrapperFunctions: node, edge
 
 
 
