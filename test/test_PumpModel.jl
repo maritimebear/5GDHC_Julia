83aead4e5d07a182bd99094143a5f18a05c6e1a3
John Mampilli <jsmampilli@gmail.com>
2024-02-13 13:05:31 +0100

    Script to test pump model

---
+++ test/test_PumpModel.jl
@@ -0,0 +1,35 @@
+# Script to test ControlFunctions.jl::PumpModel
+# Generate test domain |> Closure from PumpModel |> plot
+
+# PumpModel and reference points from Licklederer et al,
+# "Thermohydraulic model of Smart Thermal Grids with bidirectional
+# power flow between prosumers", 2021, pages 4, 12.
+
+import Plots as plt
+
+include("../src/ControlFunctions.jl")
+import .ControlFunctions: PumpModel
+
+Base.@kwdef struct ReferencePoint
+    m::Float64 # mass flow rate [kg/s]
+    dP::Float64 # pressure change [Pa]
+    n::Float64 # pump speed [rpm]
+end
+
+n_nominal = 4100.0 # nominal pump speed [rpm]
+fluid_density = 1e3 # [kg/s]
+
+ref_1 = ReferencePoint(m=0.0, dP=40221.0, n=1.0 * n_nominal)
+ref_2 = ReferencePoint(m=0.922, dP=0.0, n=1.0 * n_nominal)
+
+model = PumpModel(ref_1.m, ref_1.dP, ref_1.n,
+                  ref_2.m, ref_2.dP, ref_2.n,
+                  fluid_density, n_nominal)
+
+dx = 1e-2
+x = -ref_2.m:dx:ref_2.m # Test domain symmetric about 0, x = massflow
+y = model.(n_nominal, x)
+
+plt.plot(x, y)
+plt.xlabel!("massflow [kg/s]")
+plt.ylabel!("deltaP [Pa]")
