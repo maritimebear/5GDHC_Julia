41668c8e5ca0cfb2cd1f0757c3c929f776ccde92
John Mampilli <jsmampilli@gmail.com>
2024-02-17 15:27:15 +0100

    OK -- Churchill equation for friction factor, tested against values in Cengel Table 8-2

--- src/Transport.jl
+++ src/Transport.jl
@@ -12,9 +12,15 @@ Base.@kwdef struct TransportProperties{T1, T2, T3, T4}
 end
 
 
-function friction_Churchill(Re::Real) # -> Float64
-    # TODO
-    return # float
+function friction_Churchill(Re::Real, relative_roughness::Real) # -> Float64
+    # Re: Reynolds Number, relative_roughness = (mean pipe roughness height / pipe diameter) [m/m]
+    # Approximate explicit expression for Darcy-Weisbach friction factor
+    # Valid for laminar, transition and turbulent flows through circular sections of any roughness
+    # Cengel and Cimbala, Fluid Mechanics: Fundamentals and Applications, 4th ed., equation 8-55
+    inv_Re = 1.0 / Re
+    A = (-2.457 * log(((7.0 * inv_Re)^0.9) + (0.27 * relative_roughness)))^16
+    B = (37530.0 * inv_Re)^16
+    return 8.0 * (((8.0 * inv_Re)^12) + ((A + B)^(-3/2)))^(1/12)
 end
 
 
