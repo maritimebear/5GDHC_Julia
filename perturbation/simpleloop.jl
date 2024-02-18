6cc04cdec45da7f3433a2e21fb7dca7be8e5a2fb
John Mampilli <jsmampilli@gmail.com>
2024-02-18 12:50:39 +0100

    Renamed variables to avoid shadowing built-in function

--- perturbation/simpleloop.jl
+++ perturbation/simpleloop.jl
@@ -47,9 +47,9 @@ transport_coeffs = DHG.TransportProperties(dynamic_viscosity=dyn_visc, wall_fric
                                              heat_capacity=heat_capacity, heat_transfer=heat_transfer)
 
 ## Pipe parameters
-diameter = 1.0
-length = 1.0
-dx = 0.1
+pipe_diameter = 1.0
+pipe_length = 1.0
+pipe_dx = 0.1
 
 ## Prosumer parameters
 pump_nominalspeed = 4100.0 # rpm
@@ -93,10 +93,10 @@ node_structs = (DHG.JunctionNode(),
                )
 
 edge_structs = (
-                DHG.Pipe(1, 3, diameter, length, dx, wall_roughness),
+                DHG.Pipe(1, 3, pipe_diameter, pipe_length, pipe_dx, wall_roughness),
                 DHG.PressureChange(2, 1, producer_hydctrl, producer_thmctrl, producer_hydchar),
                 DHG.Massflow(3, 4, consumer_hydctrl, consumer_thmctrl, consumer_hydchar),
-                DHG.Pipe(4, 2, diameter, length, dx, wall_roughness),
+                DHG.Pipe(4, 2, pipe_diameter, pipe_length, pipe_dx, wall_roughness),
                )
 
 nodes = (DHG.node(x) for x in node_structs) # Generator
