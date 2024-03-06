0d6e8c7bf453f2d70493d1fd00566cbe8e1dfb00
John Mampilli <jsmampilli@gmail.com>
2024-03-06 10:53:53 +0100

    Test for temperature-dependent thermophysical properties

---
+++ cases/test/test_ThermophysicalProperties.jl
@@ -0,0 +1,37 @@
+# Script to plot variation of fluid thermophysical properties over temperature,
+# to see if models behave as expected
+
+import Plots as plt
+
+include("../src/Fluids.jl")
+
+T_range = (0 + 273.15, 200 + 273.15) # [K]
+dT = 1.0 # [K]
+
+fluid = Fluids.Water
+density = 1e3 # [kg/m^3]
+
+
+T = T_range[1] : dT : T_range[2]
+
+struct Property
+    name::String
+    unit::String
+    values::Vector{Float64}
+end
+
+names = ("Dynamic Viscosity", "Specific Heat Capacity", "Thermal Conductivity")
+units = ("Pa-s", "J/kg-K", "W/m-K")
+fns = (Fluids.dynamic_viscosity, Fluids.specific_heat, Fluids.thermal_conductivity)
+
+# Calculate thermophysical properties over temperature range
+properties = Dict(name => Property(name, unit, fn.(fluid, T))
+                  for (name, unit, fn) in zip(names, units, fns)
+                 )::Dict{String, Property}
+
+for (name, prop) in properties
+    plt.plot!(T, prop.values, label=name,
+              legendposition=:inline, legendfontsize=8,
+             )
+end
+plt.xlabel!("Temperature [K]")
